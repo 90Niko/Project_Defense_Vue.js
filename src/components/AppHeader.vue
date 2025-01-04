c1c84d8cb279bdabb869e9972ab87147dcc8ea9b
Nikolay Geroev <ngeroev78@gmail.com>
2025-01-04 13:07:16 +0100

    Update

--- src/components/AppHeader.vue
+++ src/components/AppHeader.vue
@@ -37,7 +37,7 @@ function handleLogout() {
         Welcome, {{ userName }}
       </li>
       <li v-if="isLoggedIn">
-        <button @click="handleLogout">
+        <button class="logout" @click="handleLogout">
           Logout
         </button>
       </li>
@@ -56,6 +56,13 @@ function handleLogout() {
 </template>
 
 <style scoped>
+.logout {
+  background-color: #333;
+  color: #fff;
+  border: none;
+  padding: 0.5rem 1rem;
+  cursor: pointer;
+}
 .navbar {
   text-decoration: none;
   list-style: none;
